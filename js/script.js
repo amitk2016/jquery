d987b9a65215cff3d2e9925a14afcf3e71cb56fa
Amit Kumar <amit.k03377@gmail.com>
2016-05-09 11:03:28 +1200

    finished toggle

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('h1 p').click(function(){
+		
+	});
+	$('h1').click(function(){
+		$('p').fadeOut('slow');
+	
+	});
+
+	$('#clickMe').click(function(){
+		$('.top').toggleClass('transparent');
+		
+	});
+
+
+});
+
